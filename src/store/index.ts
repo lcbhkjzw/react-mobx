d67db08c300d3cc6191da16c81f4a9cca4b12fb9
lcbhk <lcbhk@cloud.com>
2019-07-01 00:18:09 +0800

    redux dev tools

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,7 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { all } from "redux-saga/effects";
 
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const reducers = combineReducers({
   good: goodReducer
@@ -10,16 +11,16 @@ export const reducers = combineReducers({
 
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 
 function* rootSaga() {
-  yield all([
-    helloSaga(),
-    watchIncrementAsync()
-  ])
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
 const state = store.getState();
 

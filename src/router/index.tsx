8b639c245b36a58789cfad1e043a0045f3e80bc4
lcbhk <lcbhk@cloud.com>
2019-07-01 10:14:37 +0800

    prettier

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,17 @@
-import loadable from 'loadable-components';
+import loadable from "loadable-components";
 
 export const routers = [
-    {
-        path: '/login',
-        component: () => import('../views/login')
-    },
-    {
-        path: '/good',
-        component: () => import('../views/good')
-    }
+  {
+    path: "/login",
+    component: () => import("../views/login")
+  },
+  {
+    path: "/good",
+    component: () => import("../views/good")
+  }
 ].map(item => {
-    return {
-        ...item,
-        component: loadable(item.component)
-    }
-})
+  return {
+    ...item,
+    component: loadable(item.component)
+  };
+});

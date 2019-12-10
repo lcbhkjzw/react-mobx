aca154ae2bd8a6aab418f4493021f18c2923db45
lcbhk <lcbhk@icloud.com>
2019-12-10 10:01:21 +0800

    upgrade typescript

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,3 @@ const container = document.getElementById("react-app");
 
 render(<App />, container);
 
-// if (DEV && module.hot) {
-//   // Accept changes to this file for hot reloading.
-//   module.hot.accept('./index');
-//   // Any changes to our App will cause a hotload re-render.
-//   module.hot.accept(
-//     './components/App',
-//     () => renderApp(require('./components/App').default),
-//   );
-// }
